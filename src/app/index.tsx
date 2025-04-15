1eabfddb396829cc113fc2bed85b95cca22f9931
rmc-cf <rmcyyds@gamil.com>
2025-04-15 13:53:40 +0800

    Assignment:Auth

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -16,6 +16,9 @@ const index = () => {
        <Link href={'/(admin)'} asChild>
               <Button text='Admin'/>
        </Link>
+       <Link href={'/sign-in'} asChild>
+              <Button text='Sign In'/>
+       </Link>
     </View>
   )
 }
